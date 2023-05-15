a7aec27e71b647c717c0d202119350b9ae86cd30
Jana Plumm <janaplumm@outlook.com>
2023-05-15 18:40:02 +1000

    added a custom scrollbar

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
 function buttonClick() {
-    // Add your button click functionality here
-    alert("Button clicked!");
-  }
+  // Add your button click functionality here
+  alert("Button clicked!");
+}
