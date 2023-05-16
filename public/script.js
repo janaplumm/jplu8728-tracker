4a12385fca6202da5ae7b2c506f3bc1ca8b0255b
Jana Plumm <janaplumm@outlook.com>
2023-05-17 09:40:00 +1000

    added html span tags and css for emoji buttons

--- public/script.js
+++ public/script.js
@@ -36,7 +36,7 @@ stars.forEach((star) => {
         s.innerHTML = "&#9734;"; // Unicode representing empty star
       }
     });
-    console.log("Selected rating:", ratingInput.value);
+    // console.log("Selected rating:", ratingInput.value);
   });
 });
 
