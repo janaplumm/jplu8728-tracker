1cdd14978445358f9ad6a58fb6aa436edfa637a0
Jana Plumm <janaplumm@outlook.com>
2023-05-16 12:11:56 +1000

    html pop-up for input form created

--- public/script.js
+++ public/script.js
@@ -2,3 +2,19 @@ function buttonClick() {
   // Add your button click functionality here
   alert("Button clicked!");
 }
+
+// Star rating functionality
+const stars = document.querySelectorAll('.star');
+    stars.forEach(star => {
+      star.addEventListener('click', () => {
+        const rating = star.getAttribute('data-rating');
+        document.querySelector('[name="rating"]').value = rating;
+        stars.forEach(s => {
+          if (s.getAttribute('data-rating') <= rating) {
+            s.innerHTML = '&#11088;';
+          } else {
+            s.innerHTML = '&#9734;';
+          }
+        });
+      });
+    });
